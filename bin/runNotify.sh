0355077b49e649d0633f739197ba93b97c8a5e54
nri <nri@opencascade.com>
2003-05-26 15:39:43 +0000

    NRI : Remove old files. We will add again if requested.

--- bin/runNotify.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-echo "-----------------------> Daemon notification..."
-
-notifd -c ${SALOME_SRC_DIR}/resources/channel.cfg -DFactoryIORFileName=/tmp/${LOGNAME}_rdifact.ior -DChannelIORFileName=/tmp/${LOGNAME}_rdichan.ior&
-sleep 2
