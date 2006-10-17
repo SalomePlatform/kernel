8e1eb45232bf6f7e1e09ca999c105d761f12c768
srn <srn@opencascade.com>
2006-10-17 10:49:09 +0000

    IMP PAL13614: Added a new attribute AttributeString

---
+++ src/SALOMEDSClient/SALOMEDSClient_AttributeString.hxx
@@ -0,0 +1,38 @@
+// Copyright (C) 2005  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+// CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either 
+// version 2.1 of the License.
+// 
+// This library is distributed in the hope that it will be useful 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public  
+// License along with this library; if not, write to the Free Software 
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
+//  File   : SALOMEDSClient_AttributeString.hxx
+//  Author : Sergey RUIN
+//  Module : SALOME
+
+#ifndef SALOMEDSClient_AttributeString_HeaderFile
+#define SALOMEDSClient_AttributeString_HeaderFile
+
+#include "SALOMEDSClient_definitions.hxx" 
+#include "SALOMEDSClient_GenericAttribute.hxx" 
+#include <string>
+
+class SALOMEDSClient_AttributeString: public virtual SALOMEDSClient_GenericAttribute
+{
+public:  
+  virtual std::string Value() = 0;
+  virtual void SetValue(const std::string& value) = 0;
+};
+
+#endif
