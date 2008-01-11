b66c025813d0adda482367742dbe7cb137236c86
secher <secher>
2008-01-11 14:55:55 +0000

    debug of batch cases

--- bin/waitNS.sh
+++ bin/waitNS.sh
@@ -7,5 +7,11 @@ while [ $status -ne 0 ]; do
   echo -n "#"
 done
 portNumber=`grep InitRef $HOME/$APPLI/.omniORB_last.cfg | grep -v grep | awk 'BEGIN{FS=":"}{print $NF}'`
-ps -efw | grep $USER | grep omniNames | grep -v grep | grep $portNumber
+status=1
+while [ $status -ne 0 ]; do
+  ps -efw | grep $USER | grep omniNames | grep -v grep | grep $portNumber
+  status=$?
+  sleep 1
+  echo -n "="
+done
 ./runSession waitNS.py
