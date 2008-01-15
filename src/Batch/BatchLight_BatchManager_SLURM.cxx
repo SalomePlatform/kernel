1be65b5a3ad866cf1df272cdb889b44bfaa65c8d
secher <secher>
2008-01-15 15:48:04 +0000

    test on ccrt

--- src/Batch/BatchLight_BatchManager_SLURM.cxx
+++ src/Batch/BatchLight_BatchManager_SLURM.cxx
@@ -173,13 +173,12 @@ namespace BatchLight {
     tempOutputFile << "  do" << endl ;
     tempOutputFile << "    arglist=\"$arglist YACS_Server_\"$ip" << endl ;
     tempOutputFile << "  done" << endl ;
-    tempOutputFile << "  sleep 5" << endl ;
+    tempOutputFile << "  ./runSession waitNS.sh" << endl ;
     tempOutputFile << "  ./runSession waitContainers.py $arglist" << endl ;
     tempOutputFile << "  ./runSession python ~/" << _dirForTmpFiles << "/" << _fileNameToExecute << ".py" << endl;
     tempOutputFile << "  ./runSession killCurrentPort" << endl;
     tempOutputFile << "else" << endl ;
-    tempOutputFile << "  sleep 5" << endl ;
-    tempOutputFile << "  ./runSession waitNS.py" << endl ;
+    tempOutputFile << "  ./runSession waitNS.sh" << endl ;
     tempOutputFile << "  ./runSession SALOME_Container 'YACS_Server_'${SLURM_PROCID}" << endl ;
     tempOutputFile << "fi" << endl ;
     tempOutputFile.flush();
@@ -226,7 +225,7 @@ namespace BatchLight {
 
     tempOutputFile << "#! /bin/sh -f" << endl ;
     tempOutputFile << "#BSUB -n " << nbproc << endl ;
-    tempOutputFile << "#BSUB -o ~/" << _dirForTmpFiles << "/runSalome.log%J" << endl ;
+    tempOutputFile << "#BSUB -o " << _dirForTmpFiles << "/runSalome.log%J" << endl ;
     tempOutputFile << "mpirun -srun ~/" << _dirForTmpFiles << "/runSalome_" << _fileNameToExecute << "_Batch.sh" << endl ;
     tempOutputFile.flush();
     tempOutputFile.close();
