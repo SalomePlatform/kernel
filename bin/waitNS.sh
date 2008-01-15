f88624aab52796ef8551cd132b03b040f3d3232f
secher <secher>
2008-01-15 15:25:12 +0000

    test on ccrt

--- bin/waitNS.sh
+++ bin/waitNS.sh
@@ -6,12 +6,4 @@ while [ $status -ne 0 ]; do
   sleep 1
   echo -n "#"
 done
-portNumber=`grep InitRef $HOME/$APPLI/.omniORB_last.cfg | grep -v grep | awk 'BEGIN{FS=":"}{print $NF}'`
-status=1
-while [ $status -ne 0 ]; do
-  ps -efw | grep $USER | grep omniNames | grep -v grep | grep $portNumber
-  status=$?
-  sleep 1
-  echo -n "="
-done
 ./runSession waitNS.py
