47233cffe09c9deaad4aed6863a1cea9fe6c6d67
abd <abd@opencascade.com>
2008-11-26 10:26:59 +0000

    Update copyright information

--- src/Basics/SALOME_Basics.hxx
+++ src/Basics/SALOME_Basics.hxx
@@ -1,6 +1,6 @@
-//  SALOME Basics : general SALOME definitions and tools (C++ part - no CORBA)
+//  Copyright (C) 2007-2008  CEA/DEN, EDF R&D, OPEN CASCADE
 //
-//  Copyright (C) 2003  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+//  Copyright (C) 2003-2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
 //  CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
 //
 //  This library is free software; you can redistribute it and/or
@@ -17,15 +17,14 @@
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 //
-// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 //
-//
-//
+//  SALOME Basics : general SALOME definitions and tools (C++ part - no CORBA)
 //  File   : SALOME_Basics.hxx
 //  Author : Alexander A. BORODIN
 //  Module : SALOME
 //  $Header$
-
+//
 #ifndef _SALOME_BASICS_HXX_
 #define _SALOME_BASICS_HXX_
 
