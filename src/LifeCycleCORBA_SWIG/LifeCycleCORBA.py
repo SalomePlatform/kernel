394b0c601242ead71e2216b2240245361e5f1795
barate <barate>
2013-04-03 12:29:31 +0000

    Fix bug with previous integration: JobParameters constructor now takes two more parameters

--- src/LifeCycleCORBA_SWIG/LifeCycleCORBA.py
+++ src/LifeCycleCORBA_SWIG/LifeCycleCORBA.py
@@ -56,10 +56,12 @@ class ResourceParameters (Engines.ResourceParameters):
 class JobParameters (Engines.JobParameters):
   def __init__(self, job_name="", job_type="", job_file="", env_file="", in_files=[], out_files=[],
                      work_directory="", local_directory="", result_directory="", maximum_duration="",
-                     resource_required=None, queue="", specific_parameters=[]):
+                     resource_required=None, queue="", specific_parameters=[],
+                     launcher_file = "", launcher_args = ""):
     Engines.JobParameters.__init__(self, job_name, job_type, job_file, env_file, in_files, out_files,
                                          work_directory, local_directory, result_directory, maximum_duration,
-                                         resource_required, queue, specific_parameters)
+                                         resource_required, queue, specific_parameters,
+                                         launcher_file, launcher_args)
 
 class ResourceDefinition(Engines.ResourceDefinition):
   def __init__(self, name="", hostname="", protocol="rsh", username="", applipath="", componentList=[],
