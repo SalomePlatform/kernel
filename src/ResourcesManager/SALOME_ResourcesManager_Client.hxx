6ba22390ee3d60fabbb7fcfca5a0911feca55717
Renaud Barate <renaud.barate@edf.fr>
2015-01-08 10:44:06 +0100

    Fix a bug causing concurrent access problems to the Resources Manager
    Since the development of parallel container launch, the container
    manager is multi-threaded, but it used directly the resource manager
    which is not thread safe. With this fix, all calls from the container
    manager to the resource manager are done through CORBA, and since the
    resource manager runs in a single-thread POA, thread-safety is ensured.

---
+++ src/ResourcesManager/SALOME_ResourcesManager_Client.hxx
@@ -0,0 +1,52 @@
+// Copyright (C) 2007-2014  CEA/DEN, EDF R&D, OPEN CASCADE
+//
+// Copyright (C) 2003-2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+// CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
+
+#ifndef __SALOME_RESOURCESMANAGER_CLIENT_HXX__
+#define __SALOME_RESOURCESMANAGER_CLIENT_HXX__
+
+#include "SALOME_ResourcesManager_Common.hxx"
+
+class SALOME_NamingService;
+
+// This class is fully thread-safe because the ResourcesManager CORBA object
+// runs in a single-thread POA.
+class SALOMERESOURCESMANAGER_EXPORT SALOME_ResourcesManager_Client
+{
+public:
+
+  SALOME_ResourcesManager_Client(SALOME_NamingService *ns);
+  ~SALOME_ResourcesManager_Client();
+
+  std::vector<std::string> GetFittingResources(const resourceParams& params);
+  //std::string FindFirst(const std::vector<std::string>& listOfResources) const;
+  std::string Find(const std::string & policy, const std::vector<std::string> & listOfResources);
+  ParserResourcesType GetResourceDefinition(const std::string & name);
+  //void AddResource(const ParserResourcesType & new_resource, bool write, const std::string & xml_file);
+  //void RemoveResource(const std::string & name, bool write, const std::string & xml_file);
+
+protected:
+
+  Engines::ResourcesManager_var _rm;
+
+};
+
+#endif
