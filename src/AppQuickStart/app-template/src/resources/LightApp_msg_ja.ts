20debaa89b97a805869ae51a9d5d6370f1ce4e5d
Christophe Bourcier <christophe.bourcier@cea.fr>
2015-05-05 16:01:23 +0200

    Update translation files from Crowdin

---
+++ src/AppQuickStart/app-template/src/resources/LightApp_msg_ja.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja" sourcelanguage="en">
+  <context>
+    <name>@default</name>
+    <message>
+      <source>ABOUT_COPYRIGHT</source>
+      <translation>Copyright (C)</translation>
+    </message>
+    <message>
+      <source>ABOUT_CAPTION</source>
+      <translation>About [Name_of_Application]</translation>
+    </message>
+    <message>
+      <source>APP_NAME</source>
+      <translation>[Name_of_Application] [SLOGAN]</translation>
+    </message>
+    <message>
+      <source>APP_VERSION</source>
+      <translation>V[VERSION]</translation>
+    </message>
+    <message>
+      <source>ABOUT_LICENSE</source>
+      <translation>License description</translation>
+    </message>
+    <message>
+      <source>ABOUT_VERSION</source>
+      <translation>Version [VERSION]</translation>
+    </message>
+    <message>
+      <source>ABOUT_TITLE</source>
+      <translation>[Name_of_Application]</translation>
+    </message>
+  </context>
+</TS>
