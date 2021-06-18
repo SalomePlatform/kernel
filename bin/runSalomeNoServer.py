e47efe9880170ecf24845fb9133ff5f61e18523f
Ovidiu MIRCESCU <ovidiu.mircescu@edf.fr>
2021-06-18 11:22:50 +0200

    Add script to launch salome gui in session less mode.

---
+++ bin/runSalomeNoServer.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+# Copyright (C) 2021  CEA/DEN, EDF R&D
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+# See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+#
+import setenv
+import runSalome
+import os
+
+class NoSessionServer(runSalome.SessionServer):
+    def __init__(self,args,modules_list,modules_root_dir):
+        super().__init__(args,modules_list,modules_root_dir)
+        for i in range (len(self.SCMD1)):
+            if self.SCMD1[i] == "SALOME_Session_Server" :
+                self.SCMD1[i] = "SALOME_Session_Server_No_Server"
+        os.putenv("SALOME_EMB_SERVANT", "1")
+        SalomeAppSLConfig=os.getenv("SalomeAppConfig","")
+        os.putenv("SalomeAppSLConfig", SalomeAppSLConfig)
+
+if __name__ == "__main__":
+    args, modules_list, modules_root_dir = setenv.get_config()
+    mySessionServ = NoSessionServer(args, modules_list, modules_root_dir)
+    mySessionServ.setpath(modules_list, modules_root_dir)
+    mySessionServ.run()
